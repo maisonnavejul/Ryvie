7eb456a9ed25fb48d3c8190add70341baa7374c5
Jules MAISONNAVE <jules.maisonnave@student.junia.com>
2024-10-18 18:05:21 +0200

    update

--- Ryvie-Back/index.js
+++ Ryvie-Back/index.js
@@ -1,21 +1,13 @@
-// Importer le module Express
 const express = require('express');
-
-// Initialiser une application Express
+const cors = require('cors');
 const app = express();
 
-// Définir le port sur lequel le serveur écoute
-const port = 3000;
+app.use(cors()); // Autorise toutes les requêtes cross-origin
 
-// Ajouter un middleware pour traiter les requêtes JSON
-app.use(express.json());
-
-// Créer une route GET de base
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur mon serveur Express!');
+  res.status(200).send('Server is running');
 });
 
-// Démarrer le serveur
-app.listen(port, () => {
-  console.log(`Serveur Express.js en cours d'exécution sur le port ${port}`);
+app.listen(3000, () => {
+  console.log('Server running on http://192.168.1.39:3000');
 });
