61c7600d140003b99cd9fecaae7777f8fb0f6545
maisonnavejul <jules.maisonnave@gmail.com>
2024-10-21 14:51:12 +0200

    with icons activity

--- Ryvie-Front/src/Home.js
+++ Ryvie-Front/src/Home.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { io } from 'socket.io-client';
+
 // Fonction pour importer toutes les images du dossier icons
 const importAll = (r) => {
   let images = {};
@@ -14,8 +15,8 @@ const importAll = (r) => {
 };
 
 const images = importAll(require.context('./icons', false, /\.(png|jpe?g|svg)$/));
-const weatherImages = importAll(require.context('./weather_icons', false, /\.(png|jpe?g|svg)$/)); // Ajoute des images pour la météo
-const weatherIcons = importAll(require.context('./weather_icons', false, /\.(png|jpe?g|svg)$/)); // Ajoute des images pour la météo et les icônes
+const weatherImages = importAll(require.context('./weather_icons', false, /\.(png|jpe?g|svg)$/));
+const weatherIcons = importAll(require.context('./weather_icons', false, /\.(png|jpe?g|svg)$/));
 
 // Types pour react-dnd
 const ItemTypes = {
@@ -23,7 +24,7 @@ const ItemTypes = {
 };
 
 // Composant pour chaque icône avec son nom affiché sous l'icône (sauf côté gauche)
-const Icon = ({ id, src, zoneId, moveIcon, handleClick, showName = true }) => {
+const Icon = ({ id, src, zoneId, moveIcon, handleClick, showName = true, isActive }) => {
   const ref = React.useRef(null);
 
   const [{ isDragging }, drag] = useDrag({
@@ -45,16 +46,34 @@ const Icon = ({ id, src, zoneId, moveIcon, handleClick, showName = true }) => {
           backgroundImage: `url(${src})`,
           opacity: isDragging ? 0.5 : 1,
           cursor: 'pointer',
+          position: 'relative', // Nécessaire pour positionner la pastille
         }}
         onClick={() => handleClick(id)}
-      ></div>
+      >
+        {/* Pastille de notification */}
+        {id === 'Cloud.png' || id === 'Portainer.png' ? (
+          <div
+            className="status-badge"
+            style={{
+              position: 'absolute',
+              top: '-5px',
+              right: '-5px',
+              width: '16px',
+              height: '16px',
+              borderRadius: '50%',
+              backgroundColor: isActive ? 'green' : 'red',
+              border: '2px solid white', // Pour l'effet de "sur" l'icône comme sur iPhone
+            }}
+          ></div>
+        ) : null}
+      </div>
       {showName && <p className="icon-name">{id.replace('.jpeg', '').replace('.png', '')}</p>}
     </div>
   );
 };
 
 // Composant pour chaque zone
-const Zone = ({ zoneId, iconId, moveIcon, handleClick, showName }) => {
+const Zone = ({ zoneId, iconId, moveIcon, handleClick, showName, appStatus }) => {
   const [{ isOver, canDrop }, drop] = useDrop({
     accept: ItemTypes.ICON,
     canDrop: () => true,
@@ -83,6 +102,7 @@ const Zone = ({ zoneId, iconId, moveIcon, handleClick, showName }) => {
             moveIcon={moveIcon}
             handleClick={handleClick}
             showName={showName}
+            isActive={appStatus[iconId[0]]} // Passe l'état actif/inactif
           />
         )}
       </div>
@@ -144,11 +164,35 @@ const Home = () => {
 
   const [serverStatus, setServerStatus] = useState(false); // Status de la connexion au serveur
 
+  const [appStatus, setAppStatus] = useState({
+    'Cloud.png': false,
+    'Portainer.png': false,
+  });
+
   useEffect(() => {
-    const socket = io('http://192.168.1.39:3000'); // Remplacer par l'adresse correcte de votre serveur
+    const socket = io('http://192.168.1.34:3001'); // Remplacer par l'adresse correcte de votre serveur
 
     socket.on('status', (data) => {
       setServerStatus(data.serverStatus);
+      if (data.serverStatus) {
+        console.log('Connected to server');
+      }
+    });
+
+    // Ecoute des conteneurs actifs en temps réel
+    socket.on('containers', (data) => {
+      console.log('Conteneurs actifs:', data.activeContainers);
+
+      // Si "Cloud" ou "Portainer" est actif
+      const isCloudRunning = data.activeContainers.includes('Cloud');
+      const isPortainerRunning = data.activeContainers.includes('Portainer');
+
+      // Met à jour le statut en temps réel
+      setAppStatus((prevStatus) => ({
+        ...prevStatus,
+        'Cloud.png': isCloudRunning,
+        'Portainer.png': isPortainerRunning,
+      }));
     });
 
     socket.on('disconnect', () => {
@@ -162,43 +206,46 @@ const Home = () => {
     const fetchWeatherData = () => {
       const geoApiUrl = 'http://ip-api.com/json';
 
-      axios.get(geoApiUrl).then((response) => {
-        const { city, lat, lon } = response.data;
+      axios
+        .get(geoApiUrl)
+        .then((response) => {
+          const { city, lat, lon } = response.data;
 
-        const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&hourly=temperature_2m,weathercode,relative_humidity_2m,windspeed_10m&timezone=auto`;
+          const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&hourly=temperature_2m,weathercode,relative_humidity_2m,windspeed_10m&timezone=auto`;
 
-        axios.get(weatherApiUrl).then((weatherResponse) => {
-          const data = weatherResponse.data;
+          axios.get(weatherApiUrl).then((weatherResponse) => {
+            const data = weatherResponse.data;
 
-          const weatherCode = data.current_weather.weathercode;
-          let icon = 'sunny.png';
+            const weatherCode = data.current_weather.weathercode;
+            let icon = 'sunny.png';
 
-          if (weatherCode >= 1 && weatherCode <= 3) {
-            icon = 'cloudy.png';
-          } else if (weatherCode === 61 || weatherCode === 63 || weatherCode === 65) {
-            icon = 'rainy.png';
-          }
+            if (weatherCode >= 1 && weatherCode <= 3) {
+              icon = 'cloudy.png';
+            } else if (weatherCode === 61 || weatherCode === 63 || weatherCode === 65) {
+              icon = 'rainy.png';
+            }
 
+            setWeather({
+              location: city,
+              temperature: data.current_weather.temperature,
+              humidity: data.hourly.relative_humidity_2m[0],
+              wind: data.current_weather.windspeed,
+              description: weatherCode,
+              icon: icon,
+            });
+          });
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la récupération de la localisation', error);
           setWeather({
-            location: city,
-            temperature: data.current_weather.temperature,
-            humidity: data.hourly.relative_humidity_2m[0],
-            wind: data.current_weather.windspeed,
-            description: weatherCode,
-            icon: icon,
+            location: 'Localisation non disponible',
+            temperature: null,
+            humidity: null,
+            wind: null,
+            description: '',
+            icon: 'default.png',
           });
         });
-      }).catch((error) => {
-        console.error("Erreur lors de la récupération de la localisation", error);
-        setWeather({
-          location: 'Localisation non disponible',
-          temperature: null,
-          humidity: null,
-          wind: null,
-          description: '',
-          icon: 'default.png',
-        });
-      });
     };
 
     fetchWeatherData(); // Appeler la fonction au montage initial
@@ -260,12 +307,15 @@ const Home = () => {
                   iconId={zones['left']}
                   moveIcon={moveIcon}
                   handleClick={handleClick}
+                  appStatus={appStatus}
                 />
               </div>
               <div className="widget" style={{ backgroundImage: `url(${weatherImages[weather.icon]})` }}>
                 <div className="weather-info">
                   <p className="weather-city">{weather.location ? weather.location : 'Localisation non disponible'}</p>
-                  <p className="weather-temperature">{weather.temperature ? `${Math.round(weather.temperature)}°C` : '...'}</p>
+                  <p className="weather-temperature">
+                    {weather.temperature ? `${Math.round(weather.temperature)}°C` : '...'}
+                  </p>
                   <div className="weather-humidity">
                     <img src={weatherIcons['humidity.png']} alt="Humidity Icon" className="weather-icon" />
                     {weather.humidity ? `${weather.humidity}%` : '...'}
@@ -282,6 +332,7 @@ const Home = () => {
                   iconId={zones['right']}
                   moveIcon={moveIcon}
                   handleClick={handleClick}
+                  appStatus={appStatus}
                   className="zone-right"
                 />
               </div>
@@ -294,6 +345,7 @@ const Home = () => {
                   iconId={zones[`bottom${i + 1}`]}
                   moveIcon={moveIcon}
                   handleClick={handleClick}
+                  appStatus={appStatus}
                 />
               ))}
             </div>
